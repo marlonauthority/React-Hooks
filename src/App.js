9c2299d1df21e091bdc1ceb52571d9d995f59181
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-09 19:59:27 -0400

    useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [tecnologias, attTecnologia] = useState(['ReactJS', 'React Native']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    // o state ainda eé imutavel, por isso deve-se copiar tudo e atualizar seu valor posteriormente a copia spread operators
+    attTecnologia([...tecnologias, newTech]);
+    setNewTech('');
+  }
+  return (
+    <>
+      <ul>
+        {tecnologias.map(tecnologia => (
+          <li key={tecnologia}>{tecnologia}</li>
+        ))}
+      </ul>
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adcionar
+      </button>
+    </>
+  );
 }
 
 export default App;
